2c16182d6f6125d6e15b8d86e626aeb0ea2f402d
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2023-11-27 16:23:00 +0400

    add buttons and displey in html

--- javascript.js
+++ javascript.js
@@ -14,14 +14,16 @@ function multiply(a,b){
 function divide(a,b){
     return a/b
 }
+// functions for all of the basic math operators 
 
 
 let firstNumber = 1;
 let oper = "add" ;
 let secondNumber = 5;
+// variables for calculations and display
 
 
-function operator(firstNumber,secondNumber,oper){
+function operator(){
     if(oper === "add"){
         return add(firstNumber,secondNumber)
     }else if(oper ==="subtract"){
@@ -35,6 +37,7 @@ function operator(firstNumber,secondNumber,oper){
     }
 
 }
+// fonction to conect variables with math operators
 
 
 console.log(operator(firstNumber,secondNumber,oper))
